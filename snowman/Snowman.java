2d6ccbe21eaaca9f123116e091541263ed5b229e
mohammadawwad <mohd-awwad@hotmail.com>
2022-02-03 19:31:44 -0800

    snowman finished

--- snowman/Snowman.java
+++ snowman/Snowman.java
@@ -13,7 +13,7 @@ public class Snowman extends Frame{
 
     public Snowman(){  
         setTitle("Snowman");
-        setSize(300, 200);
+        setSize(600, 600);
         setVisible(true);
         addWindowListener(new WindowCloser());
     }
@@ -26,33 +26,66 @@ public class Snowman extends Frame{
     public void paint(Graphics g){
 
         //constants
-        final int MID = 150;
-        final int TOP = 50;
+        final int MID = 300;
+        final int TOP = 300;
 
         setBackground(Color.cyan);
 
-        g.setColor (Color.blue);
-        g.fillRect (0, 175, 300, 50);  // ground
+        //ground
+        g.setColor(Color.blue);
+        g.fillRect(0, 550, 600, 50);  
 
-        g.setColor (Color.yellow);
-        g.fillOval (-40, -40, 80, 80);  // sun
+        //sun
+        g.setColor(Color.yellow);
+        g.fillOval(-40, -40, 200, 200);  
 
-        g.setColor (Color.white);
-        g.fillOval (MID-20, TOP, 40, 40);      // head
-        g.fillOval (MID-35, TOP+35, 70, 50);   // upper torso
-        g.fillOval (MID-50, TOP+80, 100, 60);  // lower torso
+        g.setColor(Color.white);
+        //head
+        g.fillOval(MID-20, TOP, 90, 90);  
+        //mid body
+        g.fillOval(MID-35, TOP+80, 120, 90); 
+        //lower body
+        g.fillOval(MID-50, TOP+150, 150, 120);  
 
-        g.setColor (Color.black);
-        g.fillOval (MID-10, TOP+10, 5, 5);   // left eye
-        g.fillOval (MID+5, TOP+10, 5, 5);    // right eye
+        g.setColor(Color.black);
+        //left eye
+        g.fillOval(MID+5, TOP+25, 15, 15);   
+        //right eye
+        g.fillOval(MID+25, TOP+25, 15, 15);   
 
-        g.drawArc (MID-10, TOP+20, 20, 10, 190, 160);   // smile
+        g.setColor(Color.white);
+        //inner left eye
+        g.fillOval(MID+10, TOP+30, 5, 5); 
+        //inner right eye  
+        g.fillOval(MID+30, TOP+30, 5, 5);    
 
-        g.drawLine (MID-25, TOP+60, MID-50, TOP+40);  // left arm
-        g.drawLine (MID+25, TOP+60, MID+55, TOP+60);  // right arm
+        //nose
+        g.setColor(Color.black);
+        g.fillOval(MID+20, TOP+45, 7, 7); 
 
-        g.drawLine (MID-20, TOP+5, MID+20, TOP+5);  // brim of hat
+        //smile
+        g.drawArc(MID+5, TOP+45, 40, 20, 190, 160);   
 
-        g.fillRect (MID-15, TOP-20, 30, 25);        // top of hat
+        //left eyebrow
+        g.drawArc(MID+6, TOP+20, 13, 8, 0, 180);   
+
+        //right eyebrow
+        g.drawArc(MID+26, TOP+20, 13, 8, 0, 180);   
+
+        //left arm
+        g.drawLine(MID-25, TOP+110, MID-70, TOP+70);  
+        //right arm
+        g.drawLine(MID+70, TOP+110, MID+150, TOP+110);  
+
+        //top of hat
+        g.fillRect(MID-2, TOP-30, 50, 35);        
+        
+        //lower part of hat
+        g.drawLine(MID-15, TOP+5, MID+65, TOP+5);  
+
+        //buttons
+        g.fillOval(MID+18, TOP+100, 10, 10); 
+        g.fillOval(MID+18, TOP+115, 10, 10); 
+        g.fillOval(MID+18, TOP+130, 10, 10); 
     }
 }
