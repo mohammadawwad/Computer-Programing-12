d9fe779356b34fe122fde7090191da558947d12f
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-03-28 20:47:06 +0000

    random number generator

---
+++ printF/Formating.java
@@ -0,0 +1,8 @@
+public class Formating{
+    public static void main (String[] args){
+        String p = "%3.1f"; 
+        double q=3.567; 
+        System.out.println("Hello");
+    }
+}
+
---
+++ random/RandomNum.java
@@ -0,0 +1,18 @@
+ import java.util.Random;
+ 
+ /**
+ * Demonstrates the Random class.
+ */
+ public class RandomNum {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+				
+		System.out.println("First number: " + r.nextInt(100));
+		System.out.println("Second number: " + r.nextInt(100));
+		System.out.println("Third number: " + r.nextInt(100));
+		System.out.println("Fourth number: " + r.nextInt(100));
+		System.out.println("Fifth number: " + r.nextInt(100));
+		
+	}
+}
