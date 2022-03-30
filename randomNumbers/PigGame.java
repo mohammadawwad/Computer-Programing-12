cb40db26316783c21f8f8710002c2e4d5e34bf2e
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-03-30 17:57:36 +0000

    fixed pig game and is now working with arraylists, cleaning and seperating the code now

--- randomNumbers/PigGame.java
+++ randomNumbers/PigGame.java
@@ -2,24 +2,30 @@ package randomNumbers;
 
 import java.util.Scanner;
 import java.util.Random;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
  
 
  public class PigGame {
-
+     
+        //keeps track of users and computers total number
+        public static List<Integer> totalUserCount = new ArrayList<>();
+        public static List<Integer> totalCompCount = new ArrayList<>();
+        
+        
+        public static int userCount;
+        public static int compCount;
+        
+        //represents your dice roll numbers
+        public static int roll1;
+        public static int roll2;
+        
 	public static void main(String[] args) {
-		
-                
-                //represents your dice roll numbers
-                int userRoll1 = 0;
-                int userRoll2 = 0;
-                
-                //computer dice roll numbers
-                int compRoll1 = 0;
-                int compRoll2 = 0;
-                
-                //keeps track of users and computers total number
-                int userCount = 0;
-                int compCount = 0;
+		//initialising the games class
+                PigGame game = new PigGame();
                 
                 //last player that has rolled
                 //true for user, false for computer
@@ -30,12 +36,12 @@ import java.util.Random;
                     if(lastPlayer == true){
                         System.out.println("_________USERS TURN__________");
                         System.out.println("TEST------: " + userCount);
-                        roll(userRoll1, userRoll2, userCount);
+                        game.roll(userCount, totalUserCount);
                         lastPlayer = false;
                     }
                     else{
                         System.out.println("_________COMPUTERS TURN__________");
-                        roll(compRoll1, compRoll2, compCount);
+                        game.roll(compCount, totalCompCount);
                         lastPlayer = true;
                     }
                 }
@@ -45,45 +51,54 @@ import java.util.Random;
         
         
         //void for rolling the dice
-        public static int roll(int num1, int num2, int count){
+        public int roll(int count, List<Integer> listCounter){
             Random r = new Random();
             
-            //loops for a hundred rolls
+
             for(int x = 0; x <= 100; x++){
-                num1 = r.nextInt(6) + 1;
-                num2 = r.nextInt(6) + 1;
-                
+
+                roll1 = r.nextInt(6) + 1;
+                roll2 = r.nextInt(6) + 1;
+            
                 //outputs dice info
-                System.out.println("Your rolls are: " + num1 + " and " + num2);
+                System.out.println("Your rolls are: " + roll1 + " and " + roll2);
 
                 //if you roll a 1 you reset to zero and the other player plays their turn
-                if(num1 == 1 || num2 == 1){
+                if(roll1 == 1 || roll2 == 1){
                     System.out.println("you rolled a 1, next players turn");
                     count = 0;
+                    listCounter.clear();
                     return count;
                 }
                 else{
-                    int total = num1 + num2;
-                    count += total;
+                    listCounter.add(roll1);
+                    listCounter.add(roll2);
+                    System.out.println(listCounter);
                     
+                    //loops through list of your rolls and adds it to count
+                    for (int i: listCounter) {
+                        count += i;
+                    }
+
                     //you win if you add upto over 100
                     if(count >= 100){
                         System.out.println("You Win");
                         return count;
                     }
+                    
                     //outputs dice info and promts to go again or stop
                     System.out.println("Your Count: " + count);
                     System.out.println("Would you like to coninue:  1=yes  0=no");
-                    
+
                     Scanner input = new Scanner(System.in);  
                     if(input.nextInt() == 0){
                         return count;
                     }
-                    
 
                 }
-            }
             
+            }
+           
             return count;
         }
         
