2a2955b2f8bb09463a7cbfec1eb72a13218757a6
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-04-01 19:35:41 +0000

    indents

--- randomNumbers/MinMaxGame.java
+++ randomNumbers/MinMaxGame.java
@@ -7,21 +7,22 @@ import java.util.Random;
  public class MinMaxGame {
 
 	public static void main(String[] args) {
-                Scanner input = new Scanner(System.in);
-		Random r = new Random();
+        Scanner input = new Scanner(System.in);
+		
+        Random r = new Random();
                 
-                //gloabals variables
-                int min, max;
-                
-                System.out.println("Enter your min number: ");
-                min = input.nextInt();
-                
-                System.out.println("Enter your max number: ");
-                max = input.nextInt();
-                
-                //creates random number between 0 to 10
-                int computerNum = r.nextInt(max + 1 - min) + min;
-                System.out.println("Random Number: " + computerNum);
+        //gloabals variables
+        int min, max;
+        
+        System.out.println("Enter your min number: ");
+        min = input.nextInt();
+        
+        System.out.println("Enter your max number: ");
+        max = input.nextInt();
+        
+        //creates random number between 0 to 10
+        int computerNum = r.nextInt(max + 1 - min) + min;
+        System.out.println("Random Number: " + computerNum);
                 
 		
 	}
