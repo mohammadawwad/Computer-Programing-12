b1b7942e48165098763b75ef60eec08184badafa
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-05-18 17:30:08 +0000

    pulling 3 cards and returning a total with it

--- enumsAndSwitch/MainCards.java
+++ enumsAndSwitch/MainCards.java
@@ -4,13 +4,31 @@ public class MainCards{
     public static void main(String[] args){
         
         //initialising and setting the enum
-        ChooseEnum generateCard = new ChooseEnum();
+        ChooseEnum generateCard1 = new ChooseEnum();
+        ChooseEnum generateCard2 = new ChooseEnum();
+        ChooseEnum generateCard3 = new ChooseEnum();
+        
         
         //initilising the tyoe of card that will be pulled
-        generateCard.initializeEnum();
+        generateCard1.initializeEnum();
+        generateCard2.initializeEnum();
+        generateCard3.initializeEnum();
+        
+        
+        //casting the numbers cards you get as ints
+        int card1 = (int) generateCard1.cards.randomCard();
+        int card2 = (int) generateCard2.cards.randomCard();
+        int card3 = (int) generateCard3.cards.randomCard();
         
         //outputs the first months details
-        System.out.println("You Recieve: " + generateCard.cards.randomCard() + " of " + generateCard.cards); 
+        System.out.println("You Recieve: " + card1 + " of " + generateCard1.cards); 
+        System.out.println("You Recieve: " + card2 + " of " + generateCard2.cards); 
+        System.out.println("You Recieve: " + card3 + " of " + generateCard3.cards); 
+        
+        
+        //adding and outputting the total
+        int cardTotal = card1 + card2 + card3;
+        System.out.println("Your Total: " + cardTotal);
    
     } 
 } 
