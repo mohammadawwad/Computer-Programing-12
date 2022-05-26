d5c99b892b348f9ef5e464b2397c4286c54bf558
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-05-26 16:51:27 +0000

    latest work for hannoi algo, currently has an error around move 4 or 5 when there are no more moves on A

--- towersOfHanoi/HanoiAlgorithm.java
+++ towersOfHanoi/HanoiAlgorithm.java
@@ -6,15 +6,19 @@ import java.util.ArrayList;
 
 public class TowersOfHannoi {
     
+    
+    static int numOfDisks;
+    static int numOfMoves;
+        
     public static void main(String[] args){
         //adding three disks to pole 1
         poleA.add(1);
         poleA.add(2);
         poleA.add(3);
         
-        showOutput();
-        moveDiskFromAtoC();
-        showOutput();
+        
+        calculateMovesNum();
+        moveDiskAlgo();
     }
     
     //all the poles that will be used                         - 1   index 0 of array
@@ -22,18 +26,136 @@ public class TowersOfHannoi {
     //                                                      --- 3   index 2 of array
     //Creating the array Lists for each pole  
     static ArrayList<Integer> poleA = new ArrayList<>();  
-
     static ArrayList<Integer> poleB = new ArrayList<>();      
     static ArrayList<Integer> poleC = new ArrayList<>();  
-        
     
     
-    public static void moveDiskFromAtoC(){
-        //moving the the first disk to the last
-       int topDiskOfA = poleA.get(0);
-       poleC.add(1);
-       poleA.remove(0);
+    public static int calculateMovesNum(){
+        //calculates the number of moves needed based on the amount of starting disks
+        numOfDisks = poleA.size();
+        numOfMoves = (int) (Math.pow(2, numOfDisks) - 1);
+        return numOfMoves;
     }
+
+    public static void moveDiskAlgo() {
+//  1. Calculate the total number of moves required i.e. "pow(2, n)- 1" here n is number of disks.
+
+//  2. If number of disks (i.e. n) is even then interchange destination pole and auxiliary pole.
+
+//  3. for i = 1 to total number of moves:
+//          if i%3 == 1:
+//              legal movement of top disk between pole A and pole C
+//          if i%3 == 2:
+//              legal movement of top disk between pole A and pole B
+//          if i%3 == 0:
+//              legal movement of top disk between pole B and pole C 
+
+        System.out.println("Number Of Moves Required: " + numOfMoves);
+        
+          
+        
+           
+            for(int moveCount = 1; moveCount < 7; moveCount++){
+                
+//                calculateMovesNum();
+                
+//                if(numOfDisks % 2 == 0){
+//                    //decides where the disk will be moved too
+//                    if(moveCount % 3 == 1){
+//                        moveDisk("AtoB");
+//                        showOutput();
+//                    }
+//                    if(moveCount % 3 == 2){
+//                        moveDisk("AtoC");
+//                        showOutput();
+//                    }
+//                    if(moveCount % 3 == 0){
+//                        moveDisk("BtoA");
+//                        showOutput();
+//                    }
+//                } 
+                
+//                else {
+                    
+                    //decides where the disk will be moved too
+                    if(moveCount % 3 == 1){
+                        moveDisk("AtoC");
+                        showOutput();
+                    }
+                    if(moveCount % 3 == 2){
+                        moveDisk("AtoB");
+                        showOutput();
+                    }
+                    if(moveCount % 3 == 0){
+                        moveDisk("CtoB");
+                        showOutput();
+                    }
+//                }
+
+            }
+        
+        
+        
+    }
+    
+
+    
+    
+    public static void moveDisk(String type){
+        
+        int topDiskOfA;
+        int topDiskOfB;
+        int topDiskOfC;
+        
+        switch(type){
+            
+            case "AtoB": 
+                //moving the the first disk from A to the top of the pole B
+                topDiskOfA = poleA.get(0);
+                poleB.add(0, topDiskOfA);
+                poleA.remove(0);
+                break;
+                
+            case "AtoC": 
+                //moving the the first disk from A to the top of pole C
+                topDiskOfA = poleA.get(0);
+                poleC.add(0, topDiskOfA);
+                poleA.remove(0);
+                break;
+
+            case "BtoA": 
+                //moving the the fisrt disk from B to the top of pole A
+                topDiskOfB = poleB.get(0);
+                poleA.add(0, topDiskOfB);
+                poleB.remove(0);
+                break;
+
+            case "BtoC": 
+                //moving the the first disk from B to the top of pole C
+                topDiskOfB = poleB.get(0);
+                poleC.add(0, topDiskOfB);
+                poleB.remove(0);
+                break;
+
+
+            case "CtoA": 
+                //moving the the first disk from C to the top of pole A
+                topDiskOfC = poleC.get(0);
+                poleA.add(0, topDiskOfC);
+                poleC.remove(0);
+                break;
+
+
+            case "CtoB": 
+                //moving the the first disk from C to the top of pole B
+                topDiskOfC = poleC.get(0);
+                poleB.add(0, topDiskOfC);
+                poleC.remove(0);
+                break;
+
+        }
+    }
+    
 
     
     //outputting the numbers that represent the disks in each pole
@@ -54,11 +176,7 @@ public class TowersOfHannoi {
         for(int z = 0; z < poleC.size(); z++){
           System.out.println(poleC.get(z));
         }
-        
     }
-    
-    
-    
     
  
 }
